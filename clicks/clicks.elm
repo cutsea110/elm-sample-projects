eb6375c1654d379a002b63aefa5d2102dd66e4ed
cutsea110 <cutsea110@gmail.com>
2017-02-07 17:27:44 +0900

    succeed to build and run clicks

--- clicks/clicks.elm
+++ clicks/clicks.elm
@@ -1,9 +1,27 @@
-import Mouse
-import Signal exposing (..)
-import Html exposing (Html, text)
+import Html exposing (Html, text, div)
+import Mouse exposing (..)
 
-main : Signal Html
-main = Signal.map (\x -> text (toString x)) countClick
+main : Program Never Model Msg
+main = Html.program { init = init
+                    , view = view
+                    , update = update
+                    , subscriptions = subscriptions
+                    }
 
-countClick : Signal Int
-countClick = foldp (\clk count -> count + 1) 0 Mouse.clicks
+type alias Model = Int
+
+init : (Model, Cmd Msg)
+init = (0, Cmd.none)
+
+type Msg = Click
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Click -> (model + 1, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Mouse.clicks (\_ -> Click)
+
+view : Model -> Html Msg
+view model = Html.text (toString model)
