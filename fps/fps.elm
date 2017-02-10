880b1746c74846297d81122d95c9045d6907198f
cutsea110 <cutsea110@gmail.com>
2017-02-10 15:52:11 +0900

    succeed to build and run fps

--- fps/fps.elm
+++ fps/fps.elm
@@ -1,6 +1,30 @@
-import Graphics.Element exposing (..)
-import Signal exposing (..)
-import Time exposing (fps)
+module Main exposing (..)
 
-main : Signal Element
-main = map show (foldp (+) 0 (fps 30))
+import AnimationFrame exposing (diffs)
+import Html exposing (Html, text)
+import Time exposing (Time)
+
+main = Html.program
+       { init = init
+       , view = view
+       , update = update
+       , subscriptions = subs
+       }
+
+type alias Model = Time
+
+init : (Model, Cmd Msg)
+init = (0, Cmd.none)
+
+type Msg = Tick Time
+
+view : Model -> Html Msg
+view model = text (toString model)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Tick t -> (t, Cmd.none)
+
+subs : Model -> Sub Msg
+subs model = diffs Tick
