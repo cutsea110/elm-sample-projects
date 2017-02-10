e738e4e0d1aaff4d56477b928c9c534349886e99
cutsea110 <cutsea110@gmail.com>
2017-02-10 18:50:32 +0900

    succeed to build and run stamps

--- stamps/stamps.elm
+++ stamps/stamps.elm
@@ -1,24 +1,56 @@
+module Main exposing (..)
+
 import Color exposing (..)
-import Graphics.Collage exposing (..)
-import Graphics.Element exposing (..)
+import Collage exposing (..)
+import Element exposing (..)
+import Html exposing (..)
 import List exposing (map, (::))
 import Mouse exposing (..)
-import Signal exposing (..)
+import Task
 import Window exposing (..)
 
-main : Signal Element
-main = Signal.map2 scene Window.dimensions clickLocation
+main : Program Never Model Msg
+main = Html.program
+       { init = init
+       , view = view
+       , update = update
+       , subscriptions = subs
+       }
 
-clickLocation : Signal (List (Int, Int))
-clickLocation = Signal.foldp (::) [] (Signal.sampleOn Mouse.clicks Mouse.position)
+type alias Model = { locs : List Mouse.Position
+                   , windowSize : Window.Size
+                   }
 
-scene : (Int, Int) -> List (Int, Int) -> Element
-scene (w, h) locs =
-  let drawPentagon (x, y) =
-        ngon 5 20
-          |> filled (hsla (toFloat x) 0.9 0.6 0.7)
-          |> move (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
-          |> rotate (toFloat x)
-  in layers [ collage w h (List.map drawPentagon locs)
-            , show "Click to stamp a pentagon."
-            ]
+init : (Model, Cmd Msg)
+init = (Model [] (Window.Size 0 0), Task.perform Resize Window.size)
+
+type Msg = MouseClick Mouse.Position
+         | Resize Window.Size
+
+view : Model -> Html Msg
+view model = toHtml (elems model)
+
+elems : Model -> Element
+elems model =
+    let (w, h) = (model.windowSize.width, model.windowSize.height)
+        locs = model.locs
+        drawPentagon pos =
+            let (x, y) = (pos.x, pos.y)
+            in ngon 5 20
+                |> filled (hsla (toFloat x) 0.9 0.6 0.7)
+                |> move (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
+                |> rotate (toFloat x)
+    in layers [ collage w h (List.map drawPentagon locs)
+              , show "Click to stamp a pentagon."
+              ]
+
+subs : Model -> Sub Msg
+subs model = Sub.batch [ Window.resizes Resize
+                       , Mouse.clicks MouseClick
+                       ]
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Resize sz -> ({model | windowSize = sz}, Cmd.none)
+        MouseClick pos -> ({model | locs = model.locs ++ [pos]}, Cmd.none)
