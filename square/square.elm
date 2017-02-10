1caa2fecb67139abdd1753b621236eddd49e37a8
cutsea110 <cutsea110@gmail.com>
2017-02-10 17:43:46 +0900

    succeed to build and run square

--- square/square.elm
+++ square/square.elm
@@ -1,9 +1,15 @@
-import Color exposing (..)
-import Graphics.Collage exposing (..)
-import Graphics.Element exposing (..)
+module Main exposing (..)
 
-main : Element
-main = collage 200 200
+import Html exposing (Html)
+import Color exposing (..)
+import Collage exposing (..)
+import Element exposing (..)
+
+main : Html ()
+main = toHtml g
+
+g : Element
+g = collage 200 200
        [ filled blue (square 100)
        , filled red (square 100) |> move (toFloat 20, toFloat 20) |> rotate (toFloat 45)
        ]
