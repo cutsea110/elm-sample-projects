645c96cc98ef916ac0d7055db6f4667bb22018e0
cutsea110 <cutsea110@gmail.com>
2017-02-10 17:01:52 +0900

    succeed to build and run position

--- position/position.elm
+++ position/position.elm
@@ -1,5 +1,29 @@
-import Html exposing (Html, text)
-import Mouse
+module Main exposing (..)
 
-main : Signal Html
-main = Signal.map (text << toString) Mouse.position
+import Html exposing (..)
+import Mouse exposing (..)
+
+main = Html.program
+       { init = init
+       , view = view
+       , update = update
+       , subscriptions = subs
+       }
+
+type alias Model = Position
+
+init : (Model, Cmd Msg)
+init = ({x = 0, y = 0}, Cmd.none)
+
+type Msg = MouseMsg Position
+
+view : Model -> Html Msg
+view model = text (toString model)
+
+subs : Model -> Sub Msg
+subs model = Mouse.moves MouseMsg
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        MouseMsg pos -> (pos, Cmd.none)
