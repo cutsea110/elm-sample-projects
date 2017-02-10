49c111e6468c3f6706e586624690407708b19898
cutsea110 <cutsea110@gmail.com>
2017-02-10 17:16:52 +0900

    succeed to build and run press

--- press/press.elm
+++ press/press.elm
@@ -1,6 +1,32 @@
-import Graphics.Element exposing (..)
-import Mouse
-import Signal exposing (..)
+module Main exposing (..)
 
-main : Signal Element
-main = map show Mouse.isDown
+import Html exposing (..)
+import Mouse
+
+main = Html.program
+       { init = init
+       , view = view
+       , update = update
+       , subscriptions = subs
+       }
+
+type alias Model = Bool
+
+init : (Model, Cmd Msg)
+init = (False, Cmd.none)
+
+type Msg = MouseUp | MouseDown
+
+view : Model -> Html Msg
+view model = text (toString model)
+
+subs : Model -> Sub Msg
+subs model = Sub.batch [ Mouse.downs (\_ -> MouseDown)
+                       , Mouse.ups (\_ -> MouseUp)
+                       ]
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        MouseUp -> (False, Cmd.none)
+        MouseDown -> (True, Cmd.none)
