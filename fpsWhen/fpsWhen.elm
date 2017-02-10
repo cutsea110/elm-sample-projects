b305679145309f616b5996f794d5132832f7b370
cutsea110 <cutsea110@gmail.com>
2017-02-10 16:13:19 +0900

    succeed to build and run fpsWhen

--- fpsWhen/fpsWhen.elm
+++ fpsWhen/fpsWhen.elm
@@ -1,7 +1,42 @@
-import Graphics.Element exposing (..)
-import Mouse
-import Signal exposing (..)
-import Time exposing (fpsWhen)
+module Main exposing (..)
 
-main : Signal Element
-main = map show (foldp (+) 0 (30 `fpsWhen` Mouse.isDown))
+import AnimationFrame exposing (diffs)
+import Html exposing (Html, text)
+import Time exposing (Time)
+import Mouse
+
+main = Html.program
+       { init = init
+       , view = view
+       , update = update
+       , subscriptions = subs
+       }
+
+type alias Model = { total: Time
+                   , state: Bool
+                   }
+
+init : (Model, Cmd Msg)
+init = ({total = 0, state = False}, Cmd.none)
+
+type Dir = Up | Down
+type Msg = Tick Time | MouseMsg Dir
+
+view : Model -> Html Msg
+view model = text (toString model)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Tick t -> if model.state
+                  then ({model | total = model.total + t}, Cmd.none)
+                  else (model, Cmd.none)
+        MouseMsg Down -> ({model | state = True}, Cmd.none)
+        MouseMsg Up -> ({model | state = False}, Cmd.none)
+
+subs : Model -> Sub Msg
+subs model =
+    Sub.batch [ Mouse.downs (\_ -> MouseMsg Down)
+              , Mouse.ups (\_ -> MouseMsg Up)
+              , diffs Tick
+              ]
