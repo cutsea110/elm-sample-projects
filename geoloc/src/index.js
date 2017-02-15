46deb2b8ae7ca5fbcf672a237830df5f83db0727
cutsea110 <cutsea110@gmail.com>
2017-02-15 14:45:10 +0900

    update and fixed

--- geoloc/src/index.js
+++ geoloc/src/index.js
@@ -11,3 +11,49 @@ var mountNode = document.getElementById('main');
 
 // .embed()はオプションの第二引数を取り、プログラム開始に必要なデータを与えられる。たとえばuserIDや何らかのトークンなど
 var app = Elm.Main.embed(mountNode);
+
+var mapDiv = document.getElementById('map');
+var myLatlng = new google.maps.LatLng(0, 0);
+var mapOptions = {
+    zoom: 18,
+    center: myLatlng
+};
+
+var gmap = new google.maps.Map(mapDiv, mapOptions);
+
+var routeCoordinates = new google.maps.MVCArray();
+var routePath = new google.maps.Polyline({
+    path: routeCoordinates,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+});
+routePath.setMap(gmap);
+
+var markerList = new google.maps.MVCArray();
+
+app.ports.markerMove.subscribe(function(loc) {
+    console.log("received", loc);
+    var myLatlng = new google.maps.LatLng(loc);
+    gmap.setCenter(loc);
+    
+    markerList.forEach(function (mkr, idx) {
+	mkr.setMap(null);
+    });
+    var myMarker = new google.maps.Marker({
+	position: loc,
+	title: loc.msg,
+	icon: new google.maps.MarkerImage(
+            'lazy.png',                   // url
+            new google.maps.Size(63,63),  // size
+            new google.maps.Point(0,0),   // origin
+            new google.maps.Point(32,32), // anchor
+            new google.maps.Size(63,63)   // scaledSize
+	)
+    });
+    myMarker.setMap(gmap);
+    markerList.push(myMarker);
+	    
+    routeCoordinates.push(myLatlng);
+    console.log("routes", routeCoordinates);
+});
